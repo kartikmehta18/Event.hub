3a0dfbd9c01deeb99a7c11fc169474c8a80b1458
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2025-05-04 13:58:18 +0530

    db

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -89,6 +89,10 @@ const config: Config = {
   			'accordion-down': 'accordion-down 0.2s ease-out',
   			'accordion-up': 'accordion-up 0.2s ease-out'
   		}
+  	},
+  	container: {
+  		center: true,
+  		padding: '1rem',
   	}
   },
   plugins: [require("tailwindcss-animate")],
